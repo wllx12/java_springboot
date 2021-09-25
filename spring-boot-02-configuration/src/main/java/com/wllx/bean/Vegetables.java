a471b46882da32071c2da3898c5176253356cf23
wllx <wllx@msn.com>
2021-09-25 20:54:38 +0800

    spring-boot

---
+++ spring-boot-02-configuration/src/main/java/com/wllx/bean/Vegetables.java
@@ -0,0 +1,18 @@
+package com.wllx.bean;
+
+import lombok.Data;
+
+
+/**
+ * @Description:
+ * @Author: wllx
+ * @Date: 2021/09/22 22:47:01
+ * @Version: 1.0
+ * @Tel/QQ: 691033
+ */
+@Data
+public class Vegetables {
+    private String potato;
+    private String eggplant;
+    private String greenpeper;
+}
