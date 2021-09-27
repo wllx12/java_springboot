f93cc86b4c74a6ede91012e7868b539a49fe278b
wllx <wllx@msn.com>
2021-09-27 16:15:20 +0800

    spring-boot

---
+++ spring-boot-04-orm-jpa/src/main/java/com/wllx/controller/GustController.java
@@ -0,0 +1,32 @@
+package com.wllx.controller;
+
+import com.wllx.bean.Guest;
+import com.wllx.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: wllx
+ * @Date: 2021/09/25 12:31:22
+ * @Version: 1.0
+ * @Tel/QQ: 691033
+ */
+@Controller
+@RequestMapping("/guest")
+public class GustController {
+    @Autowired
+    private GuestService guestService;
+
+    @GetMapping
+    public String list(Model model){
+        List<Guest>guestList=guestService.list();
+        model.addAttribute("guestlist",guestList);
+        return "list";
+    }
+
+}
---
+++ spring-boot-04-orm-jpa/src/main/java/com/wllx/repository/GuestRepository.java
@@ -0,0 +1,14 @@
+package com.wllx.repository;
+
+import com.wllx.bean.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Description:
+ * @Author: wllx
+ * @Date: 2021/09/27 15:44:57
+ * @Version: 1.0
+ * @Tel/QQ: 691033
+ */
+public interface GuestRepository extends JpaRepository<Guest,Long> {
+}
