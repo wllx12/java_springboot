45e69cb7a52e813437877b8ba2097831a49fa982
wllx <wllx@msn.com>
2021-09-27 21:55:43 +0800

    spring-boot-restful-add

--- spring-boot-03-restful/src/main/java/com/wllx/controller/GuestController.java
+++ spring-boot-03-restful/src/main/java/com/wllx/controller/GuestController.java
@@ -18,7 +18,7 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/guest")
-public class GustController {
+public class GuestController {
     @Autowired
     private GuestService guestService;
 
