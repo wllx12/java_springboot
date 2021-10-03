2d5f503418da2b84d7a391a84b424644549f62b7
wllx <wllx@msn.com>
2021-10-04 00:06:48 +0800

    spring-boot-event-listener

---
+++ spring-boot-05-event-listener/src/main/java/com/wllx/EventListenerApplication.java
@@ -0,0 +1,13 @@
+package com.wllx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventListenerApplication.class, args);
+    }
+
+}
---
+++ spring-boot-05-event-listener/src/test/java/com/wllx/SpringBoot05EventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wllx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot05EventListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
