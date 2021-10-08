03b654e2006faf0e7ec265194fe10bca2278ca34
wllx <wllx@msn.com>
2021-10-08 17:29:14 +0800

    spring-boot-web-container

--- spring-boot-05-web-container/src/main/java/com/wllx/WebContainerApplication.java
+++ spring-boot-05-web-container/src/main/java/com/wllx/WebContainerApplication.java
@@ -2,12 +2,26 @@ package com.wllx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class WebContainerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebContainerApplication.class, args);
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> hello(){
+        return route(GET("/hello"),serverRequest-> ok().body(Mono.just("Hello World"),String.class));
     }
 
 }
--- spring-boot-05-web-container/src/main/java/com/wllx/config/WebConfig.java
+++ spring-boot-05-web-container/src/main/java/com/wllx/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.wllx.config;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +24,13 @@ public class WebConfig {
                 factory.setPort(8899);
 
             }
+        };
+    }
+
+    @Bean
+    public ApplicationRunner runner(WebServerApplicationContext context){
+        return args->{
+            System.out.println("WebConfig-----当前Web容器的实现类是"+context.getWebServer().getClass().getName());
         };
     }
 }
